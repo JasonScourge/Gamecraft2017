f275c3d427103bae91b95a8447f70be0ac6192f8
drtrifle <drtrifle@gmail.com>
2017-12-08 19:44:11 +0800

    Synced CameraMovement & TileMovement spped to dino animation

---
+++ Assets/Scripts/Dino.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour {
+
+    CameraMovement MainCameraScript;
+
+    public float animationSpeed = 1f;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        MainCameraScript = Camera.main.GetComponent<CameraMovement>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void MoveMainCamera() {
+        MainCameraScript.MoveCamera();
+    } 
+}
