7c5c540ca5fde0be09e7084ac1cbb62654c8f85a
jasonscourge <jasonscourge@hotmail.com>
2017-12-08 17:42:27 +0800

    Create end game menu script and scene

---
+++ Assets/Scripts/EndingMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Just in case if we modify the game speed to increase difficulty
+		Time.timeScale = 1.0f;
+	}
+	
+	public void exitGame(){
+		Application.Quit ();
+	}
+
+	public void backToMainMenu(){
+		SceneManager.LoadScene (0);
+	}
+
+	public void playAgain(){
+		SceneManager.LoadScene (1);
+	}
+}
