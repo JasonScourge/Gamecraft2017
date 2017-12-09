543b36aacdec17334c8b3f36f2c8708de00b19d3
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-12-09 12:16:08 +0800

    Added score counter

--- Assets/Scripts/Killzone.cs
+++ Assets/Scripts/Killzone.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Killzone : MonoBehaviour {
 
     public CameraMovement cameraMoveScript;
+	public Text scoreText;
+	int score = 0;
+
+	void OnGUI () {
+		scoreText.text = "Humans saved: " + score.ToString ();
+	}
+
 
 	void OnTriggerEnter2D(Collider2D collider) {
         if(collider.gameObject.GetComponent<Human>() != null) {
             cameraMoveScript.IncreaseSpeed(0.005f);
             collider.gameObject.SetActive(false);
+			score += 1;
         }
 
         if (collider.gameObject.GetComponent<LegoBlock>() != null) {
