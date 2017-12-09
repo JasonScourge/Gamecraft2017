6928afde000ca46ea586db01f133f19a66e9f127
drtrifle <drtrifle@gmail.com>
2017-12-09 09:40:40 +0800

    Fixed Dino Animation & collision detection

--- Assets/Scripts/Killzone.cs
+++ Assets/Scripts/Killzone.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Killzone : MonoBehaviour {
 
+    public CameraMovement cameraMoveScript;
+
 	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.gameObject.GetComponent<Human> () != null ||
-			collider.gameObject.GetComponent<LegoBlock>() != null) {
+        if(collider.gameObject.GetComponent<Human>() != null) {
+            cameraMoveScript.IncreaseSpeed(0.1f);
+            collider.gameObject.SetActive(false);
+        }
+
+        if (collider.gameObject.GetComponent<LegoBlock>() != null) {
 			collider.gameObject.SetActive (false);
 		}
 	}
