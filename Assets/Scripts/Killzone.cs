82f68df45b136f937f35e10a56bfd26a9f418b8e
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-12-09 08:37:32 +0800

    Created 'killzone' to set interactables outside of camera zone to inactive

---
+++ Assets/Scripts/Killzone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killzone : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.GetComponent<Human> () != null ||
+			collider.gameObject.GetComponent<LegoBlock>() != null) {
+			collider.gameObject.SetActive (false);
+		}
+	}
+}
