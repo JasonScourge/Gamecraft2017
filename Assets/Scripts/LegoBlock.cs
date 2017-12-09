e58fc03c32a3067f9e60082dbab6281c89e75b1d
drtrifle <drtrifle@gmail.com>
2017-12-09 11:58:19 +0800

    resolve conflicts

--- Assets/Scripts/LegoBlock.cs
+++ Assets/Scripts/LegoBlock.cs
@@ -12,13 +12,9 @@ public class LegoBlock : Interactables {
 	}
 
 	public override void OnSquishedByFoot () {
-<<<<<<< HEAD
         //Camera.main.GetComponent<CameraMovement> ().TriggerSlowEffect (slowAmount, slowDuration);
         Camera.main.GetComponent<CameraMovement>().IncreaseSpeed(-0.075f);
-=======
 		audioSource.PlayOneShot (legoBreakClip);
-        Camera.main.GetComponent<CameraMovement>().IncreaseSpeed(-0.05f);
->>>>>>> 79654dfce352ebd7c392e2fec8056f025c4f34e4
         base.OnSquishedByFoot ();
 	}
 }
